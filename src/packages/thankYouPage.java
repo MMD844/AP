79d5982d282cb562fd82328973417d9b1091ec33
Payam <payam85ar@gmail.com>
2025-03-01 15:39:52 +0330

    set BorderLayout to main panel and thankYouPage panel

--- src/packages/thankYouPage.java
+++ src/packages/thankYouPage.java
@@ -1,6 +1,7 @@
 package packages;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class thankYouPage {
 
@@ -8,15 +9,14 @@ public class thankYouPage {
 
         // Create and configure the panel
         JPanel panel = new JPanel();
-        panel.setLayout(null);
+        panel.setLayout(new BorderLayout());
         panel.setBounds(0, 0, 600, 600);
 
         // Create thank you label
         JLabel thankYouLabel = new JLabel("با تشکر از مشارکت شما!");
-        thankYouLabel.setBounds(10, 10, 200, 20);
 
         // Add components to the panel
-        panel.add(thankYouLabel);
+        panel.add(thankYouLabel, BorderLayout.CENTER);
 
         // Add panel to the frame
         frame.add(panel);
